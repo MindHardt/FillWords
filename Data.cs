d424c95a2cbf69c8d2cfd85fc4c570388f0abf02
MindHardt <71509337+MindHardt@users.noreply.github.com>
2020-11-08 16:57:07 +0500

    Menu 1.0 (WIP)
    MainMenu + NewGameMenu

---
+++ Data.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+
+namespace FillWordsData
+{
+    public abstract class Screen
+    {
+        public int ID;
+        public int maxRow;
+        public int selectedRow;
+        public abstract void Draw();
+        public static void SetColor()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        public static void WriteRow(string row, bool selected)
+        {
+            string output = String.Empty;
+            for (int i = 0; i < 41 - (row.Length / 2); i++) Console.Write(" ");
+            if (!selected) SetColor(); else SetColorSelected();
+            Console.Write(row);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+        public static void SetColorSelected()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    public class MainMenuScreen : Screen
+    {
+        public override void Draw()
+        {
+            Console.SetCursorPosition(0,0);
+            Console.CursorVisible = false;
+            Console.WriteLine();
+            SetColor();
+            Console.WriteLine(" ┌─────┐ ┌───┐ ┌───┐   ┌───┐   ┌───┐   ┌───┐ ┌──────┐ ┌──────┐     ┌───┐ ┌──────┐ ");
+            Console.WriteLine(" │ ┌───┘ └┐ ┌┘ └┐ ┌┘   └┐ ┌┘   └┐ ┌┘   └┐ ┌┘ │ ┌──┐ │ │ ┌──┐ │     └┐ ┌┘ │ ┌────┘ ");
+            Console.WriteLine(" │ └─┐    │ │   │ │     │ │     │ │     │ │  │ │  │ │ │ └──┘ │ ┌────┘ │  │ └────┐ ");
+            Console.WriteLine(" │ ┌─┘    │ │   │ │     │ │     │ │ ┌─┐ │ │  │ │  │ │ │ ┌─┐ ┌┘ │ ┌──┐ │  └────┐ │ ");
+            Console.WriteLine(" │ │     ┌┘ └┐ ┌┘ └──┐ ┌┘ └──┐ ┌┘ └─┘ └─┘ └┐ │ └──┘ │ │ │ │ └┐ │ └──┘ └┐ ┌────┘ │ ");
+            Console.WriteLine(" └─┘     └───┘ └─────┘ └─────┘ └───────────┘ └──────┘ └─┘ └──┘ └───────┘ └──────┘ ");
+            Console.ResetColor();
+            Console.WriteLine();
+            WriteRow("Новая Игра", (selectedRow == 1));
+            Console.WriteLine();
+            WriteRow("Продолжить", (selectedRow == 2));
+            Console.WriteLine();
+            WriteRow("Рейтинг", (selectedRow == 3));
+            Console.WriteLine();
+            WriteRow("Выход", (selectedRow == 4));
+        }
+    }
+    public class NewGameScreen : Screen
+    {
+        public override void Draw()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.CursorVisible = false;
+            Console.WriteLine();
+            WriteRow(" Новая Игра ", false);
+            Console.WriteLine();
+            WriteRow(" Для продолжения введите своё имя: ", selectedRow == 1);
+            Console.WriteLine();
+            WriteRow("Назад", (selectedRow == 2));
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,102 @@
-﻿using System;
+﻿using FillWordsData;
+using System;
 
 namespace FillWords
 {
     class Program
     {
+        static int activeScreen = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "FillWords by Un1ver5e";
+            Console.SetWindowSize(83, 40);
+            Console.SetBufferSize(83, 40);
+            MainMenuScreen MainMenu1 = new MainMenuScreen();
+            InitializeScreen(MainMenu1, 4, 1);
+            NewGameScreen NewGameMenu1 = new NewGameScreen();
+            InitializeScreen(NewGameMenu1, 2, 2);
+            while (true)
+            {
+                DrawNeeded(activeScreen, MainMenu1, NewGameMenu1);
+                ConsoleKey CK = Console.ReadKey(true).Key;
+                switch (activeScreen)
+                {
+                    case 1:
+                        Scroll(CK, MainMenu1);
+                        if (CK == ConsoleKey.Enter) ExecuteButtonAction(MainMenu1);
+                        break;
+                    case 2:
+                        Scroll(CK, NewGameMenu1);
+                        if (CK == ConsoleKey.Enter) ExecuteButtonAction(NewGameMenu1);
+                        break;
+                    default: break;
+                }
+            }
+
+
         }
+        static void DrawNeeded(int screen, MainMenuScreen MainMenu1, NewGameScreen NewGameMenu1)
+        {
+            switch (screen)
+            {
+                case 1:
+                    MainMenu1.Draw();
+                    break;
+                case 2:
+                    NewGameMenu1.Draw();
+                    break;
+
+
+                default: break;
+
+            }
+        }
+        static void Scroll(ConsoleKey CK, Screen Screen)
+        {
+            if ((CK == ConsoleKey.W | CK == ConsoleKey.UpArrow) & Screen.selectedRow > 1               ) Screen.selectedRow -= 1;
+            if ((CK == ConsoleKey.S | CK == ConsoleKey.DownArrow) & Screen.selectedRow < Screen.maxRow ) Screen.selectedRow += 1;
+        }
+        static void ExecuteButtonAction(Screen Screen)
+        {
+            if (Screen.ID == 1)
+            {
+                if (Screen.selectedRow == 1)
+                {
+                    Console.Clear();
+                    activeScreen = 2;
+                }
+                if (Screen.selectedRow == 2)
+                {
+                    Console.WriteLine("ЗДЕСЬ СКОРО ЧТО-ТО БУДЕТ!");
+                }
+                if (Screen.selectedRow == 3)
+                {
+                    Console.WriteLine("ЗДЕСЬ СКОРО ЧТО-ТО БУДЕТ!");
+                }
+                if (Screen.selectedRow == 4) Environment.Exit(0);
+            }
+            if (Screen.ID == 2)
+            {
+                if (Screen.selectedRow == 1)
+                {
+                    Console.WriteLine("ЗДЕСЬ СКОРО ЧТО-ТО БУДЕТ!");
+                }
+                if (Screen.selectedRow == 2)
+                {
+                    Console.Clear();
+                    activeScreen = 1;
+                }
+            }
+
+        }
+        static void InitializeScreen(Screen Screen, int maxRow, int ID)
+        {
+            Screen.maxRow = maxRow;
+            Screen.selectedRow = 1;
+            Screen.ID = ID;
+        }
+
     }
+
+
 }
